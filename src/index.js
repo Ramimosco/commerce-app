f2aa3ea33eea8cfce96480854e3ca8c7f9ec617b
Ramiro <r.mscoso@gmail.com>
2021-01-06 22:23:44 -0300

    cambios

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <p>sdasd</p>
+    <p>sdasdasdasd</p>
   </React.StrictMode>,
   document.getElementById('root')
 );
