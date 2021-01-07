3a940ab9ae43ed30fced6e12bc506556bea0b355
Ramiro <r.mscoso@gmail.com>
2021-01-06 22:24:34 -0300

    CAMBIOS 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MI COMMERCE APP
+          MI COMMERCE APPP
         </p>
         <a
           className="App-link"
