d5ff1761a8368c70a19379fc1ffe99e0bac018a5
Ramiro <r.mscoso@gmail.com>
2021-01-11 22:18:15 -0300

    navbar prueba

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './navbar.css'
+
+const NavBar = () =>{
+    return (
+        <div className="navbar">
+            <div className="logo">
+                LOGO
+            </div>
+            <ul className="listItems">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">item2</a></li>
+                <li><a href="#">item3</a></li>
+                <li><a href="#">CART</a></li>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
